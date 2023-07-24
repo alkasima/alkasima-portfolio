5361a39db82d57b8df3ef90fa41618a845fec211
Kasim Abubakar Jajere <alkasima@gmail.com>
2023-07-24 15:35:27 +0100

    Adding project two

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Title from '../layouts/Title'
-import { projectOne} from "../../assets/index";
+import { projectOne, projectTwo} from "../../assets/index";
 import ProjectsCard from './ProjectsCard';
 
 const Projects = () => {
@@ -15,16 +15,33 @@ const Projects = () => {
           des="My Projects"
         />
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 xl:gap-14">
-        <ProjectsCard
-          title="Alkatube"
-          des=" Alkatube: Discover trending movies, top-rated movies, and celebrity favorites in this dynamic movie app, expertly crafted designed with Reactjs and Tailwind Css  powered by themoviedb API.."
-          src={projectOne}
-          githubLink="https://github.com/alkasima/alkatube"
-          websiteLink="https://alkatube.netlify.app/"
+    
+      
+      <div className="flex flex-wrap mr-10 pl-5">
+  <div className="w-full md:w-1/2">
+    <ProjectsCard
+      title="Alkatube"
+      des="Alkatube: Discover trending movies, top-rated movies, and celebrity favorites in this dynamic movie app, expertly crafted designed with Reactjs and Tailwind CSS powered by themoviedb API.."
+      src={projectOne}
+      githubLink="https://github.com/alkasima/alkatube"
+      websiteLink="https://alkatube.netlify.app/"
+    />
+  </div>
 
-        />
-      </div>
+  <div className="w-full md:w-1/2">
+    <ProjectsCard
+      title="Flashjobs"
+      des="Flashjobs: Flashjobs is a sleek job board application designed with Laravel and Tailwind CSS. It features a powerful job search filter and secure user authentication for a seamless job-seeking experience..."
+      src={projectTwo}
+      githubLink="https://github.com/alkasima/flashjobs"
+      websiteLink="https://flashjobs.fly.dev/"
+    />
+  </div>
+</div>
+
+
+
+      
         
     </section>
   );
